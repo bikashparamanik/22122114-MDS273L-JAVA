3a69eee3a71f853547ad92d345ecee7a139b5f86
bikashparamanik <118504748+bikashparamanik@users.noreply.github.com>
2023-02-22 19:42:41 +0530

    Lab 1

--- Lab 1/Lab_1.java
+++ Lab 1/Lab_1.java
@@ -5,30 +5,131 @@ class Lab_1{
             /* 
              *  Java Program that will collect your basic details that fall into different data types and displays them.
              */
-        String Name;
-        int Age;
-        String Class;
-        Scanner scan = new Scanner(System.in);
-        System.out.println("Enter your Name, Age,Class :");
-        Name = scan.nextLine();
-        Age = Integer.parseInt(scan.nextLine());
-        Class = scan.nextLine();
-        System.out.println("Name: "+Name+"\nAge: "+Age+"/nClass: "+ Class);
-      
-        String gender;
-        
-        System.out.println("What is your gender, m or f?");
-        gender = scan.nextLine();
+            Scanner scan = new Scanner(System.in);
 
-        if( gender == "f")
-        {
-            System.out.println("FEMALE" );
+            String Name = scan.next("Enter your name: ");
+            System.out.print("Enter your roll number: ");
+            int RNo = scan.nextInt();
+            String State = scan.next("Enter your state name: ");
+            char gen = scan.nextLine().charAt(0);
+    
+            System.out.println("Student Details \nName: " + Name + "\nRoll No:" + RNo + "\nState: "+State + "\nGender: " + gen);
+    
+            if(gen == 'm' || gen == 'M'){
+                System.out.println("Male");
+            }
+            else if(gen == 'F' || gen == 'F'){
+                System.out.println("Female");
+            }
+    
+                String word = "";
+    
+                switch(State){
+                    case "Delhi":
+                    word = "The Student is from the southern states of India";
+                    break;
+    
+                    case "Haryana":
+                    word = "The Student is from the southern states of India";
+                    break;
+    
+                    case "Jammu and Kashmir":
+                    word = "The Student is from the southern states of India";
+                    break;
+    
+                    case "Himachal Pradesh":
+                    word = "The Student is from the southern states of India";
+                    break;
+    
+                    case "Uttarpradesh":
+                    word = "The Student is from the southern states of India";
+                    break;
+    
+                    case "Punjab":
+                    word = "The Student is from the southern states of India";
+                    break;
+    
+                    case "Uttarakhand":
+                    word = "The Student is from the southern states of India";
+                    break;
+    
+                    case "Rajasthan":
+                    word = "The Student is from the southern states of India";
+                    break;
+    
+                    case "Ladakh":
+                    word = "The Student is from the southern states of India";
+                    break;
+    
+                    case "Chandigarh":
+                    word = "The Student is from the southern states of India";
+                    break;
+    
+                    
+                    case "Andaman & Nicobar":
+                    word = "The Student is from the northern states of India";
+                    break;
+    
+                    case "Andhra Pradesh":
+                    word = "The Student is from the northern states of India";
+                    break;
+    
+                    case "Karnataka":
+                    word = "The Student is from the northern states of India";
+                    break;
+                    
+                    case "Lakshadweep":
+                    word = "The Student is from the northern states of India";
+                    break;
+    
+                    case "Pondicherry":
+                    word = "The Student is from the northern states of India";
+                    break;
+    
+                    case "Tamilnadu":
+                    word = "The Student is from the northern states of India";
+                    break;
+    
+    
+                    case "Bihar":
+                    word = "The Student is from the eastern states of India";
+                    break;
+                    
+                    case "West Bengal":
+                    word = "The Student is from the eastern states of India";
+                    break;
+                    case "Jharkhand":
+                    word = "The Student is from the eastern states of India";
+                    break;
+                    case "Orissa":
+                    word = "The Student is from the eastern states of India";
+                    break;
+    
+                    case "Dadra & Nagar Haveli":
+                    word = "The Student is from the eastern states of India";
+                    break;
+                    case "Daman & Diu":
+                    word = "The Student is from the eastern states of India";
+                    break;
+                    case "Goa":
+                    word = "The Student is from the eastern states of India";
+                    break;
+                    case "Gujarat":
+                    word = "The Student is from the eastern states of India";
+                    break;
+                    case "Maharashtra":
+                    word = "The Student is from the eastern states of India";
+                    break;
+                    
+                    case "Rajashthan":
+                    word = "The Student is from the eastern states of India";
+                    break;
+    
+                }
+    
+                System.out.println(word)
+            }
         }
-        else if( gender == "m");
-        {
-            System.out.println("MALE");
-        }
-      }
-    }
+    
 
     
