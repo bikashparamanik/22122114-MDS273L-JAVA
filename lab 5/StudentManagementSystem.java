2503bbadc780724af906d4cd8be1839767ee4b03
bikashparamanik <bikash.paramanik@msds.christuniversity.in>
2023-03-18 16:28:12 +0530

    lab 5

--- lab 5/StudentManagementSystem.java
+++ lab 5/StudentManagementSystem.java
@@ -1,5 +1,6 @@
 import java.util.Scanner;
 
+// Define the Student class
 class Student {
      String regNo;
      String name;
@@ -8,7 +9,10 @@ class Student {
      String studentClass;
      String department;
 
+
+     // Define the constructor for the student class
     public Student(String regNo, String name, String email, String phone, String studentClass, String department) {
+       //Initialize the fields with the provided value
         this.regNo = regNo;
         this.name = name;
         this.email = email;
@@ -16,6 +20,8 @@ class Student {
         this.studentClass = studentClass;
         this.department = department;
     }
+
+    // Define a method to print the details of a student
 
     public void printDetails() {
         System.out.println("Reg. No.: " + regNo);
@@ -35,25 +41,30 @@ class Student {
     }
 }
 
+// Define the main class
 public class StudentManagementSystem {
 
+  // Create an array to hold up to 100 students
     static int MAX_STUDENTS = 100;
     static Student[] students = new Student[MAX_STUDENTS];
+    // Initialize a counter to keep track of how many students have been added to the array
     static int numStudents = 0;
 
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
-
+     //  Start a loop that will continue until the user chooses to exit
         while (true) {
+            // Display the menu options to the user
             System.out.println("....................Menu..............");
             System.out.println("Enter 1 to add a student");
             System.out.println("Enter 2 to search for a student");
             System.out.println("Enter 3 to display all students");
             System.out.println("Enter 4 to exit");
             System.out.println("Please enter your choice....");
+            // Read the user's choice from the input
             int choice = scan.nextInt();
 
-
+         // use a switch statement to handle the user's choice
             switch (choice) {
                 case 1:
                     addStudent(scan);
@@ -74,6 +85,7 @@ public class StudentManagementSystem {
     }
 
      static void addStudent(Scanner scanner) {
+        // Ask the user to enter the details of the new student
         System.out.println("Enter student details:");
         System.out.print("Reg. No.: ");
         String regNo = scanner.next();
@@ -87,20 +99,23 @@ public class StudentManagementSystem {
         String studentClass = scanner.next();
         System.out.print("Department: ");
         String department = scanner.next();
-
+         
+        // Create a new Student object with the entered details
         Student student = new Student(regNo, name, email, phone, studentClass, department);
+        // Increment the counter to indicate that a new student has been added
         students[numStudents++] = student;
-
+        // Notify the user that the student has been added successfully
         System.out.println("Student added successfully");
     }
 
      static void searchStudent(Scanner scanner) {
+        // Ask the user to enter the Reg. No. or Name of the student to search for
         System.out.println("Enter student name or reg. no.: ");
         String query = scanner.next();
-
+        // Loop through the array of students to find the matching student
         for (int i = 0; i < numStudents; i++) {
             Student student = students[i];
-
+        // If a matching student is found, print
             if (student.getName().equalsIgnoreCase(query) || student.getRegNo().equalsIgnoreCase(query)) {
                 student.printDetails();
                 return;
